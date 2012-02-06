6f6cb41a77950c379b554ee79f57d6bb316aae91
Thomas Dorey <thomasdorey@mediatech.fr01.awl.atosorigin.net>
2012-02-06 15:07:07 +0100

    Test and edit TreeMap library (JS)

---
+++ assets/js/data.js
@@ -0,0 +1,52 @@
+//init data json
+var json2 = {
+  "children": [
+    {
+      "children": [],
+      "data": {
+        "playcount": "",
+        "$color": "#FF0000",
+        "image": "",
+        "$area": 40
+      },
+      "id": "Business",
+      "name": "Business"
+    },
+    {
+      "children": [],
+      "data": {
+        "playcount": "",
+        "$color": "#FFFF00",
+        "image": "",
+        "$area": 20
+      },
+      "id": "Music",
+      "name": "Music"
+    },
+    {
+      "children": [],
+      "data": {
+        "playcount": "",
+        "$color": "#00FF00",
+        "image": "",
+        "$area": 35
+      },
+      "id": "Sport",
+      "name": "Sport"
+    },
+    {
+      "children": [],
+      "data": {
+        "playcount": "",
+        "$color": "#0000FF",
+        "image": "",
+        "$area": 15
+      },
+      "id": "Misc",
+      "name": "Misc"
+    }
+  ],
+  "data": {},
+  "id": "Interests",
+  "name": "Interests"
+}
